cf85f8c7c53539e5bc4b9a733522bf6942b579c5
JACOB <JACOBPRY@AOl.com>
2019-10-15 11:36:51 -0400

    Added file structure along with NavBar

---
+++ src/components/navBar/NavBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom"
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-light fixed-center light-blue flex-md-nowrap p-0 shadow center">
+                <ul className="nav nav-pills">
+                     <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/machines">Machines</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/users">User Profile</Link>
+                        </li>
+                     {/* Button for Log Out */}
+                </ul>
+            </nav>
+                )
+            }
+        }
+
+export default NavBar
